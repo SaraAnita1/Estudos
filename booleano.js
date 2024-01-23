d10977be483f8a4d9a050cb55686a8416870d35a
Sara Anita <sara.anita.desa@gmail.com>
2024-01-23 17:01:53 -0300

    estudando tipos booleanos

---
+++ booleano.js
@@ -0,0 +1,13 @@
+// true or false
+const primeiroNum = 5;
+const segundoNum = 5;
+let cadastroAtivado = true;
+
+console.log(primeiroNum === segundoNum);
+
+// = -> atribuindo valor, == -> comparando valor, === -> verificando se um valor é estritamente igual ao outro
+
+const texto1 = 'alura';
+const texto2 = "Alura";
+
+console.log(texto1 === texto2);
