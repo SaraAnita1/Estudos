859ef13f0b44c57a4df55aaf70fd2dc93dc6593a
Sara Anita <sara.anita.desa@gmail.com>
2024-01-23 17:08:27 -0300

    exemplos

--- booleano.js
+++ booleano.js
@@ -11,3 +11,11 @@ const texto1 = 'alura';
 const texto2 = "Alura";
 
 console.log(texto1 === texto2);
+
+
+const nome1 = "lara"
+const nome2 = "ayumi"
+
+const nomeCompleto = nome1 + nome2;
+
+console.log(nomeCompleto);
