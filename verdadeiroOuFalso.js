0b50db124d292c1394dc7ec349fe718ba114d962
Sara Anita <sara.anita.desa@gmail.com>
2024-01-27 19:30:27 -0300

    revisão

---
+++ verdadeiroOuFalso.js
@@ -0,0 +1,16 @@
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(minhaVar)
+console.log(varNull)
+
+
+// 0 === ""
+// "" === false
+// 0 => false
+// 1 => true
+
+// null representa vazio
