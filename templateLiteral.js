b111d9aff191808753179a64cc69f2e0cb711df1
Sara Anita <sara.anita.desa@gmail.com>
2024-01-30 19:18:43 -0300

    template string e seu uso

---
+++ templateLiteral.js
@@ -0,0 +1,11 @@
+// template String
+
+const nome = "Sara";
+const idade = 2023 - 1997;
+const cidadeDeNascimento = "Itaquera"
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na Cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e eu nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao)
