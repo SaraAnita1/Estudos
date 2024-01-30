c5665980e807faf17c6b5acf7f588610f705d9be
Sara Anita <sara.anita.desa@gmail.com>
2024-01-30 19:45:46 -0300

    funções e como funcionam

---
+++ funções.js
@@ -0,0 +1,23 @@
+// let x = "";
+// console.log(x)
+// x = "oi";
+
+//DECLARAÇÃO DE FUNÇÃO
+
+//1º momento quando declara a função
+function imprimeTexto(texto) {
+  console.log(texto)
+}
+
+// 2º momento quando se executa a função
+
+imprimeTexto(soma());
+// imprimeTexto("outro texto");
+
+// tres formas de escrever funções
+
+function soma() {
+  return 2 + 2
+}
+
+// console.log(soma())
