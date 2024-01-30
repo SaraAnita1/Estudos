1bfe28ca388e8bab75bfb22274548b3ebfd5d8ef
Sara Anita <sara.anita.desa@gmail.com>
2024-01-30 19:06:45 -0300

    conhecendo operadores ternarios

---
+++ operadorTernario.js
@@ -0,0 +1,15 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+//if clássico
+// if (idadeCliente >= idadeMinima) {
+//   console.log("cerveja")
+// } else {
+//   console.log("suco")
+// }
+
+//ternario
+//estrutura =     condição                //true     //false
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")
+
+//usar em comparações mais curtas que não exigem muita coisa
